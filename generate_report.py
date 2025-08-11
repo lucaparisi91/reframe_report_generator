82714e5eba4823af5302fa5859b1f493162499e1
lucaparisi91 <luca.parisi91@gmail.com>
2025-08-11 09:39:46 +0100

    Added mecanism to generate a report

--- generate_report.py
+++ generate_report.py
@@ -13,19 +13,29 @@ class reportGenerator:
             return f"Failed: {testcase['fail_phase']}"
 
     def _get_perfs(self,testcase):
-        records=[]
-        for key,value in testcase["perfvalues"].items():
-            record= {
-                "Variable": key.split(":")[2],
-                "Value": value[0],
-                "Reference": value[1],
-                "Unit" : value[4] 
-            }
-            records.append(record)
-        return records
+        """ Extract performance values from a testcase. 
+        
+        Args:
+            testcase (dict): A dictionary representing a single test case from the report.
+        Returns:
+            list: A list of dictionaries, each containing performance variable, value, reference, and unit
+        """
 
+        records=[]
+
+        if "perfvalues" in testcase.keys():        
+            for key,value in testcase["perfvalues"].items():
+                record= {
+                    "Variable": key.split(":")[2],
+                    "Value": value[0],
+                    "Reference": value[1],
+                    "Unit" : value[4] 
+                }
+                records.append(record)
+            return records
+    
     def generate(self, report_type="pass" ):
-        data = []
+        records= []
         for run in self.report["runs"]:
             for testcase in run["testcases"]:
 
@@ -36,13 +46,16 @@ class reportGenerator:
 
                 if report_type == "pass":
                     record.update( {"result": self._get_result(testcase)   })
+                    records.append(record)
+                elif report_type == "performance":
 
-                data.append(record)
+                    for perf_record in self._get_perfs(testcase):
+                        current_record = record.copy()
+                        current_record.update(perf_record)
+                        records.append(current_record)
 
-                self._get_perfs(testcase)
 
-
-        return pd.DataFrame(data)
+        return pd.DataFrame(records)
 
 
 if __name__ == "__main__":
@@ -51,7 +64,7 @@ if __name__ == "__main__":
 
     with open("report.json", "r") as f:
         j=json.load(f)
-
+    
     reporter = reportGenerator(j)
-    data= reporter.generate(report_type="pass")
+    data= reporter.generate(report_type="performance")
     print(data.to_csv(sep=" "))
